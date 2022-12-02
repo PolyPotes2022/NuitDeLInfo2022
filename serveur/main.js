ef95af072dfa2ce67cc62eb52354b18a99836010
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2022-12-02 07:10:26 +0100

    sous-diviser ts

--- serveur/main.js
+++ serveur/main.js
@@ -50,12 +50,10 @@ registerAPI(app);
 app.use('/js/', (req, res, next) => sendFile(res, 'text/js', '/web/js/' + req.path, next));
 app.use('/css/', (req, res, next) => sendFile(res, 'text/css', '/web/css/' + req.path, next));
 app.use('/images/', (req, res, next) => sendFile(res, 'text/css', '/web/images/' + req.path, next));
-app.use('/ts/', (req, res, next) => {
-    if (!req.path)
-        sendFile(res, 'text/html', '/web/ts/ts.html', next);
-    else
-        sendFile(res, 'text/html', '/web/ts/' + req.path, next);
-});
+app.use('/ts/scripts/', (req, res, next) => sendFile(res, 'text/js', '/ts/scripts/' + req.path, next));
+app.use('/ts/styles/', (req, res, next) => sendFile(res, 'text/css', '/ts/styles/' + req.path, next));
+app.use('/ts/images/', (req, res, next) => sendFile(res, 'text/css', '/ts/images/' + req.path, next));
+app.use('/ts/', (req, res, next) => sendFile(res, 'text/html', '/web/ts/ts.html', next));
 app.use('/', (req, res, next) => {
     if (req.path == '/') {
         sendFile(res, 'text/html', '/web/index.html', next);
